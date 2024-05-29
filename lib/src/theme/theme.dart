8b04f52c23129e4e1ea9aa42715c300f0a74fdff
Mahima Manik <mahimamanik.22@gmail.com>
2024-05-29 12:08:29 +0530

    Add theme file

---
+++ lib/src/theme/theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+ThemeData lightMode = ThemeData.light().copyWith(
+  brightness: Brightness.light,
+);
+
+ThemeData darkMode = ThemeData.dark().copyWith(
+  brightness: Brightness.dark,
+);
