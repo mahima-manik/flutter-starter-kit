f5bd6b2c12aea64ebb12cdfa8062d371dc72c1c6
Mahima Manik <mahimamanik.22@gmail.com>
2024-05-30 20:16:14 +0530

    Set the theme colors

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -2,14 +2,27 @@
 
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool isDarkMode = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home Page'),
+        backgroundColor: Theme.of(context).colorScheme.primary, 
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.brightness_4), onPressed: () {
+            setState(() {
+              isDarkMode = !isDarkMode;
+            });
+          })
+        ],
       ),
       body: Center(
         child: Text('Home Page'),
--- lib/src/theme/theme.dart
+++ lib/src/theme/theme.dart
@@ -2,8 +2,18 @@ import 'package:flutter/material.dart';
 
 ThemeData lightMode = ThemeData(
   brightness: Brightness.light,
+  colorScheme: ColorScheme.light(
+    background: Colors.grey.shade400,
+    primary: Colors.grey.shade300,
+    secondary: Colors.grey.shade200,
+  ),
 );
 
 ThemeData darkMode = ThemeData(
   brightness: Brightness.dark,
+  colorScheme: ColorScheme.dark(
+    background: Colors.grey.shade900,
+    primary: Colors.grey.shade800,
+    secondary: Colors.grey.shade700,
+  ),
 );
