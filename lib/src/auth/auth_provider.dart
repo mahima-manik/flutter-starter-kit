ec1d4aac7b6121a1ac7aff5d13037df1b659f788
Mahima Manik <mahimamanik.22@gmail.com>
2024-06-09 22:24:57 +0530

    Change auth_service location

--- lib/src/auth/auth_provider.dart
+++ lib/src/auth/auth_provider.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-import 'auth_service.dart';
+import '../services/auth_service.dart';
 
 class UserAuthProvider extends ChangeNotifier {
   User? _user;
--- lib/src/services/auth_service.dart
+++ lib/src/services/auth_service.dart
@@ -4,7 +4,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
-import '../services/storage_service.dart';
+import 'storage_service.dart';
 
 
 class AuthService {
