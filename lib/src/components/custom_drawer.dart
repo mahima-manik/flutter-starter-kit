406c52be5211cee46017b4b42357d74da34d4f41
Mahima Manik <mahimamanik.22@gmail.com>
2024-06-11 15:43:10 +0530

    Simplify custom drawer

--- lib/src/components/custom_drawer.dart
+++ lib/src/components/custom_drawer.dart
@@ -12,64 +12,59 @@ class CustomDrawer extends StatelessWidget {
     return Drawer(
       child: Column(
         children: [
-          Expanded(
-            child: ListView(
+          DrawerHeader(
+            child: Column(
               children: [
-                DrawerHeader(
-                child: Column(
-                  children: [
-                    Expanded(
-                      child: user?.photoURL != null
-                        ? CircleAvatar(
-                            radius: 30,
-                            child: ClipOval(
-                              child: Image.network(
-                                user!.photoURL!,
-                                fit: BoxFit.cover,
-                                width: 60,
-                                height: 60,
-                                errorBuilder: (context, error, stackTrace) => Icon(Icons.person, size: 30, color: Theme.of(context).colorScheme.onSecondary),
-                              ),
-                            ),
-                          )
-                        : CircleAvatar(
-                            radius: 30,
-                            backgroundColor: Theme.of(context).colorScheme.secondary,
-                            child: Icon(Icons.person, size: 30, color: Theme.of(context).colorScheme.onSecondary),
+                Expanded(
+                  child: user?.photoURL != null
+                    ? CircleAvatar(
+                        radius: 30,
+                        child: ClipOval(
+                          child: Image.network(
+                            user!.photoURL!,
+                            fit: BoxFit.cover,
+                            width: 60,
+                            height: 60,
+                            errorBuilder: (context, error, stackTrace) => Icon(Icons.person, size: 30, color: Theme.of(context).colorScheme.onSecondary),
                           ),
-                    ),
-                  ],
-                ),
-              ),
-              ListTile(
-                leading: const Icon(Icons.home),
-                title: const Text('Home'),
-                onTap: () => Navigator.pop(context),
-              ),
-              ListTile(
-                leading: const Icon(Icons.person),
-                title: const Text('Edit Profile'),
-                onTap: () {
-                  Navigator.pop(context);
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => ProfilePage()));
-                },
+                        ),
+                      )
+                    : CircleAvatar(
+                        radius: 30,
+                        backgroundColor: Theme.of(context).colorScheme.secondary,
+                        child: Icon(Icons.person, size: 30, color: Theme.of(context).colorScheme.onSecondary),
+                      ),
               ),
             ],
           ),
           ),
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: ListTile(
-                leading: Icon(Icons.logout, color: Theme.of(context).colorScheme.onSurface.withOpacity(0.5)),
-                title: Text('Logout', style: TextStyle(color: Theme.of(context).colorScheme.onSurface.withOpacity(0.5))),
-                onTap: () {
-                  FirebaseAuth.instance.signOut();
-                  Navigator.pop(context); // Close the drawer
-                },
-              ),
+          ListTile(
+            contentPadding: const EdgeInsets.only(left: 30),
+            leading: const Icon(Icons.home),
+            title: const Text('Home'),
+            onTap: () => Navigator.pop(context),
           ),
-        ],
-      ),
+          ListTile(
+            contentPadding: const EdgeInsets.only(left: 30),
+            leading: const Icon(Icons.person),
+            title: const Text('Edit Profile'),
+            onTap: () {
+                Navigator.pop(context);
+                Navigator.push(context, MaterialPageRoute(builder: (context) => ProfilePage()));
+            },
+          ),
+          const Spacer(),
+          ListTile(
+            contentPadding: const EdgeInsets.all(30),
+            leading: Icon(Icons.logout, color: Theme.of(context).colorScheme.onSurface.withOpacity(0.5)),
+            title: Text('Logout', style: TextStyle(color: Theme.of(context).colorScheme.onSurface.withOpacity(0.5))),
+            onTap: () {
+              FirebaseAuth.instance.signOut();
+              Navigator.pop(context); // Close the drawer
+              },
+            ),
+          ],
+        ),
     );
   }
 }
