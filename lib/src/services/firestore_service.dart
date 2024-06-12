f3927d0e2062ebf58a5be5ab2537f2d350c3b4f6
Mahima Manik <mahimamanik.22@gmail.com>
2024-06-12 17:41:03 +0530

    Add basic firestore service

---
+++ lib/src/services/firestore_service.dart
@@ -0,0 +1,23 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import '../models/product.dart';
+
+class FirestoreService {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  Future<List<Product>> fetchAllProducts() async {
+    try {
+      QuerySnapshot querySnapshot = await _firestore.collection('products').get();
+      return querySnapshot.docs.map((doc) {
+        return Product(
+          name: doc['name'],
+          description: doc['description'],
+          price: doc['price'].toDouble(),
+          rating: doc['rating'].toDouble(),
+          images: List<String>.from(doc['images']),
+        );
+      }).toList();
+    } catch (e) {
+      throw Exception('Failed to fetch products: $e');
+    }
+  }
+}
