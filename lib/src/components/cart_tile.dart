5da4feab534ba2c61ae084b1ea39eb89aaeb5855
Mahima Manik <mahimamanik.22@gmail.com>
2024-06-19 10:38:55 +0530

    Add padding to Cart Tile

--- lib/src/components/cart_tile.dart
+++ lib/src/components/cart_tile.dart
@@ -15,25 +15,33 @@ class CartTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      leading: Image.network(cartItem.product.images[0], width: 50, height: 50),
-      title: Text(cartItem.product.name),
-      subtitle: Text('${cartItem.quantity} item(s)'),
-      trailing: FittedBox(
-        fit: BoxFit.fitWidth,
-        child: Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            IconButton(
-              icon: const Icon(Icons.remove),
-              onPressed: onRemove,
-            ),
-            Text('\$${cartItem.totalPrice.toStringAsFixed(2)}'),
-            IconButton(
-              icon: const Icon(Icons.add),
-              onPressed: onAdd,
-            ),
-          ],
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ListTile(
+        contentPadding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+        tileColor: Theme.of(context).colorScheme.surface,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        leading: Image.network(cartItem.product.images[0], width: 50, height: 50),
+        title: Text(cartItem.product.name),
+        subtitle: Text('${cartItem.quantity} item(s)'),
+        trailing: FittedBox(
+          fit: BoxFit.fitWidth,
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              IconButton(
+                icon: const Icon(Icons.remove),
+                onPressed: onRemove,
+              ),
+              Text('\$${cartItem.totalPrice.toStringAsFixed(2)}'),
+              IconButton(
+                icon: const Icon(Icons.add),
+                onPressed: onAdd,
+              ),
+            ],
+          ),
         ),
       ),
     );
