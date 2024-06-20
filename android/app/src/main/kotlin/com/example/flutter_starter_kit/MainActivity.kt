50933cbb18598adc3f1d502c68b3d62fc2f9544b
Mahima Manik <mahimamanik.22@gmail.com>
2024-06-20 21:38:55 +0530

    Android stripe flutter changes

--- android/app/src/main/kotlin/com/example/flutter_starter_kit/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_starter_kit/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.flutter_starter_kit
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterFragmentActivity() {
+}
