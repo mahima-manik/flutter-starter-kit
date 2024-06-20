a0ee2c4e82e74adcb86dd3faaa75c18b01744ae6
Mahima Manik <mahimamanik.22@gmail.com>
2024-06-20 16:35:30 +0530

    Add function to create customer

--- lib/src/services/payment_service.dart
+++ lib/src/services/payment_service.dart
@@ -6,27 +6,62 @@ import 'package:http/http.dart' as http;
 
 class PaymentService {
   Future<void> initPaymentSheet(String amount, String currency) async {
-    try {
-      // 1. create payment intent on the server
-      final paymentIntentData = await _createPaymentIntent(amount, currency);
-      print(paymentIntentData);
+    // Create a customer
+    final customerData = await _createStripeCustomer('test@gmail.com', 'Test User', 'Test Address');
+    final customerId = customerData['id'];
+    
+    // Create payment intent
+    final paymentIntentData = await _createPaymentIntent(amount, 'USD', customerId);
 
-      // 2. initialize the payment sheet
-      await Stripe.instance.presentPaymentSheet().then((value) {
-        print(value);
-      }).catchError((error) {
-        print(error);
-      });
-    } catch (e) {
-      print(e);
-    }
+    // Initialize the payment sheet
+    await Stripe.instance.initPaymentSheet(
+      paymentSheetParameters: SetupPaymentSheetParameters(
+        paymentIntentClientSecret: paymentIntentData['client_secret'],
+        merchantDisplayName: dotenv.env['MERCHANT_NAME'],
+        customerId: customerId,
+        customerEphemeralKeySecret: paymentIntentData['ephemeralKey'],
+      ),
+    );
+
+    // Present the payment sheet
+    await Stripe.instance.presentPaymentSheet().then((value) {
+      print('Payment succeeded: $value');
+    }).catchError((error) {
+      print('Error presenting payment sheet: $error');
+    });
   }
-  Future<Map<String, dynamic>> _createPaymentIntent(String amount, String currency) async {
+
+  Future<Map<String, dynamic>> _createStripeCustomer(String email, String name, String address) async {
+    var body = {
+      'email': email,
+      'name': name,
+      'address[line1]': address,
+      'address[city]': 'San Francisco',
+      'address[state]': 'CA',
+      'address[postal_code]': '94101',
+      'address[country]': 'US',
+      'phone': '+1234567890',
+    };
+
+    var response = await http.post(
+      Uri.parse('https://api.stripe.com/v1/customers'),
+      headers: {
+        'Authorization': 'Bearer ${dotenv.env['STRIPE_SECRET_KEY']}',
+        'Content-Type': 'application/json'
+      },
+      body: body,
+    );
+    return json.decode(response.body);
+  }
+
+  Future<Map<String, dynamic>> _createPaymentIntent(String amount, String currency, String customerId) async {
     try {
       // Request body
       Map<String, dynamic> body = {
         'amount': amount,
         'currency': currency,
+        'customer': customerId,
+        'description': 'Payment for the test project services'
       };
 
       // Make post request to Stripe
